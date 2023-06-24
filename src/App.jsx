d7ea2219d3f37da06b3c740e425238057cea1b9c
pavlo.zaporozkyi <pavlo.zaporozkyi@mev.com>
2023-06-24 15:55:24 +0300

    Add stuff

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
           <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
+          <h2>TEst</h2>
       </header>
       <MainContent />
     </>
